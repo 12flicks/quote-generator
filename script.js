2be7c2edcbac743587f5ea9324c1a12d5b2a4159
Martin Svensson <martin@pop-os.localdomain>
2022-05-28 19:33:17 +0200

    API functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('test');
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+    // Pick a random quote from apiQoutes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On load 
+getQuotes();
