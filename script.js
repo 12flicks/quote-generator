aa380ad8cf0401d677e8ff7c8960055f38a0000f
Martin Svensson <martin@pop-os.localdomain>
2022-05-29 17:37:46 +0200

    Loading animation

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+// Show Loading
+function load() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loader
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+    load();
     // Pick a random quote from apiQoutes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -26,12 +40,15 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
     
+    // Set quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    load();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
